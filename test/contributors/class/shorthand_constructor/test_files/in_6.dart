7e137b14f3a2785d9ac4bc58e0b586b0f40f404c
Nikos Portolos <nikos.portolos@gmail.com>
2022-11-14 01:03:59 +0200

    #76 Contributor tests: ignore initial comments

---
+++ test/contributors/class/shorthand_constructor/test_files/in_6.dart
@@ -0,0 +1,9 @@
+/// This is a comment
+/// that should not be ignored
+class Model {
+  final String field1;
+  final bool field2;
+  final int field3;
+}
+
+// Should not ignore documentation comments in the beginning of the file
---
+++ test/contributors/enum/constructor/test_files/in_ignore_comments.dart
@@ -0,0 +1,9 @@
+enum Enumeration {
+  value1(),
+  value2(),
+  value3();
+
+  final bool value;
+}
+
+// Should ignore comments in the beginning of the file
