151103e937251782edba22486596dca84b5b097e
Nikos Portolos <nikos.portolos@gmail.com>
2022-12-09 03:43:55 +0200

    #154 Add logging

--- test/contributors/assists/contributor_assists_test.dart
+++ test/contributors/assists/contributor_assists_test.dart
@@ -8,6 +8,7 @@ import 'package:data_class_plugin/src/contributors/class/class_contributors.dart
 import 'package:data_class_plugin/src/contributors/common/to_string_assist_contributor.dart';
 import 'package:data_class_plugin/src/contributors/enum/enum_contributors.dart';
 import 'package:data_class_plugin/src/extensions/annotation_extensions.dart';
+import 'package:data_class_plugin/src/tools/logger/plugin_logger.dart';
 import 'package:data_class_plugin/src/visitors/visitors.dart';
 import 'package:path/path.dart';
 
@@ -26,9 +27,12 @@ final String enumAnnotationPath = join(testFilesPath, 'enum_annotation.dart');
 final String unionAnnotationPath = join(testFilesPath, 'union_annotation.dart');
 final String dataClassAnnotationPath = join(testFilesPath, 'data_class_annotation.dart');
 
+class MockLogger extends PluginLogger {}
+
 void main() async {
   final DataClassPlugin plugin = DataClassPlugin(
     PhysicalResourceProvider.INSTANCE,
+    MockLogger(),
   );
 
   final AnalysisContextCollection analysis = AnalysisContextCollection(
